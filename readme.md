225bb0d497834d86b4ffc3f083aa0e1bb6d190c0
Chimezirim Victor Ekemam <ekemammezez@gmail.com>
2023-06-11 12:21:28 +0200

    gracefully handle exceptions and invalid input types. Make main class clean and robust

--- readme.md
+++ readme.md
@@ -4,6 +4,9 @@
 
 Chimezirim Victor EKEMAM
 
+## Environnement de développement
+- Langage de programmation: Java
+- Version: 17.0.6
 
 ## Exécution
 Il suffit d'exécuter le fichier Main.java, de préférence dans l'IDE Intellij.
@@ -11,8 +14,12 @@ Il suffit d'exécuter le fichier Main.java, de préférence dans l'IDE Intellij.
 ## Documentation:
 
 Main.java
-- Contient la méthode principale utilisée pour l'exécution de l'application. Toutes les variables d'entrée sont prises au moment de l'exécution dans la méthode principale et transmises à la classe JanportHoover.
+- Contient la méthode main utilisée pour l'exécution de l'application. Toutes les variables d'entrée sont prises au moment de l'exécution dans la méthode principale à l'aide de méthodes supplémentaires dans la classe omme décrit ci-dessous et transmises à la classe JanportHoover.
 - Affiche la position finale et l'orientation de l'aspirateur à la fin de l'exécution.
+- getGridSize : Récupère la taille des grilles pour les axes x et y de la pièce.
+- getInitialPosition:Récupère la position initiale de l'aspirateur dans la pièce.
+- getInitialOrientation : Récupère l'orientation initiale de l'aspirateur dans la pièce.
+- getInstructions : Récupère la série d'instructions à exécuter.
 
 JanportHoover.java
 - Le constructor de JanportHoover initialise les propriétés de la classe en fonction des valeurs fournies par l'utilisateur.
@@ -21,3 +28,5 @@ JanportHoover.java
 - rotateLeft: Tourne l'aspirateur de 90 degrés vers la gauche par rapport à son orientation actuelle.
 - moveForward: Déplace l'aspirateur d'un pas vers l'avant par rapport à son orientation actuelle.
 - La classe contient également les getters nécessaires.
+
+Note : Le système a été conçu de manière à pouvoir traiter avec élégance les cas limites tels que les entrées erronées ou non valides de l'utilisateur sans que le système ne s'arrête. Certaines contraintes ont également été prises en compte par discrétion dans la mise en œuvre.
--- src/Main.java
+++ src/Main.java
@@ -9,53 +9,17 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
 
             System.out.println("Saisir la taille de la grille de la pièce......");
-
-            System.out.print("x eg 10: ");
-            int gridSizeX = scanner.nextInt();
-
-            System.out.print("y eg 10: ");
-            int gridSizeY = scanner.nextInt();
+            int gridSizeX = getGridSize("x", scanner);
+            int gridSizeY = getGridSize("y", scanner);
 
             System.out.println("Saisir la position initiale et l'orientation de l'aspirateur...");
-            System.out.print("Coordonnée initiale x eg 5: ");
-            int initialX = scanner.nextInt();
-            while ((initialX < 0) || (initialX > gridSizeX - 1)) {
-                System.out.println("La position initiale x ne peut pas être en dehors des limites de la pièce : entre 0 et " + (gridSizeX - 1));
-                System.out.print("Coordonnée initiale x eg 5: ");
-                initialX = scanner.nextInt();
-            }
+            int initialX = getInitialPosition("x", scanner, gridSizeX);
+            int initialY = getInitialPosition("y", scanner, gridSizeY);
 
-            System.out.print("Coordonnée initiale y eg 5:");
-            int initialY = scanner.nextInt();
-            while ((initialY < 0) || (initialY > gridSizeY - 1)) {
-                System.out.println("La position initiale y ne peut pas être en dehors des limites de la pièce : entre 0 et " + (gridSizeY - 1));
-                System.out.print("Coordonnée initiale y eg 5: ");
-                initialY = scanner.nextInt();
-            }
 
-            System.out.print("Orientation initiale eg N: ");
-            char initialOrientation = Character.toUpperCase(scanner.next().charAt(0));
-            ArrayList<Character> acceptableCoordinates = new ArrayList<>();
-            acceptableCoordinates.add('N');
-            acceptableCoordinates.add('E');
-            acceptableCoordinates.add('S');
-            acceptableCoordinates.add('W');
-            while (!acceptableCoordinates.contains(initialOrientation)) {
-                System.out.println("L'orientation ne peut être que N, S, E ou W.");
-                System.out.print("Orientation initiale eg N: ");
-                initialOrientation = scanner.next().charAt(0);
-            }
+            char initialOrientation = getInitialOrientation(scanner);
 
-            System.out.println("Saisir les instructions eg DADADADAA: ");
-            String instructions;
-            while (true) {
-                System.out.print("Saisir les instructions (seuls D, G et A peuvent être contenus dans la séquence d'instructions): ");
-                instructions = scanner.next().toUpperCase();
-                if (instructions.matches("[DGA]+")) {
-                    break;
-                }
-                System.out.println("Instructions non valides. Veuillez saisir uniquement les caractères \"D\", \"G\" et \"A\".");
-            }
+            String instructions = getInstructions(scanner);
 
             scanner.close();
 
@@ -63,15 +27,77 @@ public class Main {
             hoover.runInstructions(instructions);
 
             System.out.println();
-            System.out.println();
-            System.out.println("Position finalw: x=" + hoover.getX() + " y=" + hoover.getY() +
-                    " orientation=" + hoover.getOrientation());
+            System.out.println("Position finale: x=" + hoover.getX() + ", y=" + hoover.getY() +
+                    ", orientation=" + hoover.getOrientation());
         } catch (Exception e) {
             System.out.println("Une erreur s'est produite. Veuillez vous assurer de saisir les types de données attendus pour chacune des entrées.");
             if (!e.getMessage().isEmpty()){
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public static int getGridSize(String axis, Scanner scanner){
+        System.out.print(axis +" eg 10: ");
+        int gridSize = 0;
+        try{
+            gridSize= scanner.nextInt();
+        }catch (Exception e){
+            System.out.println("La taille de la grille pour l'axe "+ axis + " doit être un nombre entier");
+            scanner.nextLine();
+            gridSize = getGridSize(axis, scanner);
+        }
+        return gridSize;
+    }
+
+    public static int getInitialPosition(String axis, Scanner scanner, int axisGridSize){
+        System.out.print("Coordonnée initiale " + axis + " eg 5: ");
+        int initial = 0;
+        try{
+            initial = scanner.nextInt();
+        }catch (Exception e){
+            System.out.println("Coordonnée initiale "+ axis + " doit être un nombre entier");
+            scanner.nextLine();
+            initial = getInitialPosition(axis, scanner, axisGridSize);
+        }
+        while ((initial < 0) || (initial > axisGridSize - 1)) {
+            System.out.println("La position initiale x ne peut pas être en dehors des limites de la pièce : entre 0 et " + (axisGridSize - 1));
+            System.out.print("Coordonnée initiale x eg 5: ");
+            scanner.nextLine();
+            initial = getInitialPosition(axis, scanner, axisGridSize);
+        }
+        return initial;
+    }
+
+    public static char getInitialOrientation(Scanner scanner){
+        System.out.print("Orientation initiale eg N: ");
+        char initialOrientation = Character.toUpperCase(scanner.next().charAt(0));
+        ArrayList<Character> acceptableCoordinates = new ArrayList<>();
+        acceptableCoordinates.add('N');
+        acceptableCoordinates.add('E');
+        acceptableCoordinates.add('S');
+        acceptableCoordinates.add('W');
+        while (!acceptableCoordinates.contains(initialOrientation)) {
+            System.out.println("L'orientation ne peut être que N, S, E ou W.");
+            System.out.print("Orientation initiale eg N: ");
+            initialOrientation = Character.toUpperCase(scanner.next().charAt(0));
+        }
+
+        return initialOrientation;
+    }
+
+    public static String getInstructions(Scanner scanner){
+        System.out.println("Saisir les instructions eg DADADADAA: ");
+        String instructions;
+        while (true) {
+            System.out.print("Saisir les instructions (seuls D, G et A peuvent être contenus dans la séquence d'instructions): ");
+            instructions = scanner.next().toUpperCase();
+            if (instructions.matches("[DGA]+")) {
+                break;
+            }
+            System.out.println("Instructions non valides. Veuillez saisir uniquement les caractères \"D\", \"G\" et \"A\".");
+        }
+        return instructions;
     }
 
 }
